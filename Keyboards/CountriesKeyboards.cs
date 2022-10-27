7c7a1799401524a7e3147f531fbe4f47aca27a9d
olegtititele <96022759+olegtititele@users.noreply.github.com>
2022-10-27 22:54:54 +0300

    Update CountriesKeyboards.cs

--- Keyboards/CountriesKeyboards.cs
+++ Keyboards/CountriesKeyboards.cs
@@ -8,15 +8,15 @@ namespace Bot_Keyboards
         // Клавиатура сервисов
         public static InlineKeyboardMarkup CountriesSitesKb = new(new []
         {
+//             new []
+//             {
+//                 InlineKeyboardButton.WithCallbackData(text: "🇶🇦 olx.qa 🇶🇦", callbackData: "olx.qa"),
+//                 InlineKeyboardButton.WithCallbackData(text: "🇴🇲 olx.com.om 🇴🇲", callbackData: "olx.com.om"),
+//             },
             new []
             {
-                InlineKeyboardButton.WithCallbackData(text: "🇶🇦 olx.qa 🇶🇦", callbackData: "olx.qa"),
-                InlineKeyboardButton.WithCallbackData(text: "🇴🇲 olx.com.om 🇴🇲", callbackData: "olx.com.om"),
-            },
-            new []
-            {
-                InlineKeyboardButton.WithCallbackData(text: "olx.com.bh", callbackData: "olx.com.bh"),
-                InlineKeyboardButton.WithCallbackData(text: "olx.com.kw", callbackData: "olx.com.kw"),
+//                 InlineKeyboardButton.WithCallbackData(text: "olx.com.bh", callbackData: "olx.com.bh"),
+                InlineKeyboardButton.WithCallbackData(text: "🇰🇼 olx.com.kw 🇰🇼", callbackData: "olx.com.kw"),
             },
 
             // new []
